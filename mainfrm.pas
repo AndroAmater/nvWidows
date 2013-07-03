1ef6a392f6973b9a9b4b61708beb6ac1581f1b38
AndroAmater <molekula2@gmail.com>
2013-07-03 14:52:25 +0200

    Medium Update
    Added:
    - Lists directory files
    - Settings Button in View Menu
    - Renamed File Menu to View
    - Resized Default Window Size
    
    Removed:
    - Settings Button

--- mainfrm.pas
+++ mainfrm.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ComCtrls,
-  StdCtrls, ExtCtrls, ActnList, Menus;
+  StdCtrls, ExtCtrls, ActnList, Menus, DbCtrls, FileCtrl;
 
 type
 
@@ -16,28 +16,31 @@ type
     ListBox1: TListBox;
     MainMenu1: TMainMenu;
     Memo1: TMemo;
-    FileMenu: TMenuItem;
+    SettingsMenu: TMenuItem;
+    ViewMenu: TMenuItem;
     OpenEditor: TMenuItem;
     Panel1: TPanel;
-    SettingsButton: TButton;
     SearchButton: TButton;
     SearchEdit: TEdit;
     Timer1: TTimer;
     ToolBarMain: TToolBar;
     procedure Action1Execute(Sender: TObject);
     procedure FileInEditorClick(Sender: TObject);
-    procedure FileMenuClick(Sender: TObject);
+    procedure SettingsMenuClick(Sender: TObject);
+    procedure ViewMenuClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
     procedure OpenEditorClick(Sender: TObject);
-    procedure SettingsButtonClick(Sender: TObject);
     procedure Timer1Timer(Sender: TObject);
   private
     { private declarations }
   public
     { public declarations }
+    procedure FileSearch(const PathName : string) ;
   end;
 
 var
   FormMain: TFormMain;
+  test:integer;
 
 implementation
 
@@ -47,6 +50,22 @@ Uses
 {$R *.lfm}
 
 { TFormMain }
+
+
+procedure TFormMain.FileSearch(const PathName : string) ;
+ var Rec : TSearchRec;
+   Path : string;
+ begin
+   test:=FindFirst (PathName, faAnyFile - faDirectory - faHidden, Rec);
+   if test = 0 then
+     try
+     repeat
+       ListBox1.Items.Add(Rec.Name) ;
+     until FindNext(Rec) <> 0;
+     finally
+       FindClose(Rec) ;
+   end;
+ end; 
 
 procedure TFormMain.Action1Execute(Sender: TObject);
 begin
@@ -58,47 +77,54 @@ begin
   FormDFEditor.Show;
 end;
 
-procedure TFormMain.FileMenuClick(Sender: TObject);
+procedure TFormMain.SettingsMenuClick(Sender: TObject);
+begin
+  FormSettings.Show;
+end;
+
+procedure TFormMain.ViewMenuClick(Sender: TObject);
 begin
 
 end;
+
+procedure TFormMain.FormCreate(Sender: TObject);
+begin
+  FileSearch('/Users/AndroAmater/Dropbox/Apps/Notes/*');
+end;
+
+
 
 procedure TFormMain.OpenEditorClick(Sender: TObject);
 begin
   FormDFEditor.Show;
 end;
 
-procedure TFormMain.SettingsButtonClick(Sender: TObject);
-begin
-  FormSettings.Show;
-end;
-
 procedure TFormMain.Timer1Timer(Sender: TObject);
 begin
-  if FormMain.Width < 500 then
-    FormMain.Width:=500;
-  if FormMain.Height < 320 then
-     FormMain.Height:=320;
+  if FormMain.Width < 800 then
+    FormMain.Width:=800;
+  if FormMain.Height < 400 then
+     FormMain.Height:=400;
 
   if ListBox1.Height < FormMain.Height - 26 then
     ListBox1.Height:=FormMain.Height - 26
   else if ListBox1.Height > FormMain.Height - 26 then
     ListBox1.Height:=FormMain.Height - 26;
 
-  if Memo1.Width < FormMain.Width - 131 then
-    Memo1.Width:=FormMain.Width - 131
-  else if Memo1.Width > FormMain.Width - 131 then
-    Memo1.Width:=FormMain.Width - 131;
+  if Memo1.Width < FormMain.Width - 400 then
+    Memo1.Width:=FormMain.Width - 400
+  else if Memo1.Width > FormMain.Width - 400 then
+    Memo1.Width:=FormMain.Width - 400;
 
   if Memo1.Height < FormMain.Height - 26 then
     Memo1.Height:=FormMain.Height - 26
   else if Memo1.Height > FormMain.Height - 26 then
     Memo1.Height:=FormMain.Height - 26;
 
-  if SearchEdit.Width < FormMain.Width - 76 * 2 then
-    SearchEdit.Width:=FormMain.Width - 76 * 2
-  else if SearchEdit.Width > FormMain.Width - 76 * 2 then
-    SearchEdit.Width:=FormMain.Width - 76 * 2;
+  if SearchEdit.Width < FormMain.Width - 76 then
+    SearchEdit.Width:=FormMain.Width - 76 
+  else if SearchEdit.Width > FormMain.Width - 76 then
+    SearchEdit.Width:=FormMain.Width - 76;
 end;
 
 
